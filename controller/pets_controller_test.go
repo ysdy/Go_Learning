387b237625a768eda788b659f590bca6d5b5ac13
Y Yoshida <aeolianharp.op10@gmail.com>
2020-04-13 02:43:13 +0000

    Add Unit Test

--- controller/pets_controller_test.go
+++ controller/pets_controller_test.go
@@ -2,17 +2,29 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
-	"github.com/ysdy/Go_Learning/db"
+	"github.com/ysdy/Go_Learning/mocks"
+	"github.com/ysdy/Go_Learning/service"
 	"net/http/httptest"
 	"testing"
 )
 
 func TestPetList(t *testing.T) {
-	// DBが必要
-	db.Init()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
 	c, _ := gin.CreateTestContext(httptest.NewRecorder())
-	var controller PetController
+
+	serviceMock := mocks.NewMockPetService(ctrl)
+	u := []service.Pet{
+		{Id: "test_id1", Name: "test name1"},
+		{Id: "test_id2", Name: "test name2"},
+	}
+
+	serviceMock.EXPECT().List().Return(u, nil)
+	controller := PetController{Petservice: serviceMock}
+
 	controller.List(c)
 	assert.Equal(t, 200, c.Writer.Status())
 }
